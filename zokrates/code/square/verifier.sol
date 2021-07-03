b584d08efb1aaa9a443fb79d9f587aaee05b3d3c
Bilgin <kocakbilgin@gmail.com>
2021-07-04 00:25:31 +0300

    fix2

--- zokrates/code/square/verifier.sol
+++ zokrates/code/square/verifier.sol
@@ -1,6 +1,6 @@
 // This file is LGPL3 Licensed
 
-/**
+/** 
  * @title Elliptic curve operations on twist points for alt_bn128
  * @author Mustafa Al-Bassam (mus@musalbas.com)
  * @dev Homepage: https://github.com/musalbas/solidity-BN256G2
